e0218e47e535a8baee2b8a61a3ce80d9c798b3b0
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-03-12 17:19:04 +0800

    chore: move into own profile

---
+++ nvim.lazy/lua/config/keymaps.lua
@@ -0,0 +1,17 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.keymap.set("n", "<leader><CR>", ":w<CR>:so %<CR>")
+vim.keymap.set("n", "<leader>\\", ":edit ~/.config/nvim/TODO.md<CR>")
+vim.keymap.set("i", "…", "<esc>")
+
+-- Move Line up and down using opt-j and opt-k
+vim.keymap.set("v", "∆", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "˚", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "∆", "ddp")
+vim.keymap.set("n", "˚", "o<esc>kkddpk")
+
+-- Remap right-shift(which is remapped by karabiner to delete forward, which is the key ctrl + D)
+-- to <Del> key
+vim.keymap.set("i", "<C-D>", "<Del>")
+vim.keymap.set("i", "jk", "<esc>")
