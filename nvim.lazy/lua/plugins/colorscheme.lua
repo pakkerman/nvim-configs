037baac1f0ee7d49e246e555cd13a4079bc5bd52
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-03-20 10:06:08 +0800

    feat: change theme

--- nvim.lazy/lua/plugins/colorscheme.lua
+++ nvim.lazy/lua/plugins/colorscheme.lua
@@ -1,16 +1,21 @@
 return {
-  -- add gruvbox
-  {
-    "ellisonleao/gruvbox.nvim",
-    "rebelot/kanagawa.nvim",
-    "sainnhe/gruvbox-material",
-    "folke/tokyonight.nvim",
-  },
-
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa-wave",
+      transparent = false,
+      colorscheme = "tokyonight-night",
     },
   },
 }
+-- return {
+--   -- add gruvbox
+--   { "ellisonleao/gruvbox.nvim" },
+--
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "gruvbox",
+--     },
+--   },
+-- }
