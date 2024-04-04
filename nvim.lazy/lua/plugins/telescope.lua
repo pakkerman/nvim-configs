db3745c5259bf054ea6090a366e3fe142998ebba
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-04-04 17:45:08 +0800

    feat: config plugins

---
+++ nvim.lazy/lua/plugins/telescope.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    -- disable the keymap to grep files
+    { "<leader><space>", false },
+    -- change a keymap
+    { "<leader>p", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+    { "<leader>r", "<cmd>Telescope resume<cr>", desc = "Resume" },
+    -- add a keymap to browse plugin files
+    -- {
+    --   "<leader>fp",
+    --   function()
+    --     require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+    --   end,
+    --   desc = "Find Plugin File",
+    -- },
+  },
+}
