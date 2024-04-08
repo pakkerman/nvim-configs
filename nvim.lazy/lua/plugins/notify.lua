778c6384f638d3ef043297483805aa0c7672879e
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-04-08 09:10:08 +0800

    feat: nice

--- nvim.lazy/lua/plugins/notify.lua
+++ nvim.lazy/lua/plugins/notify.lua
@@ -19,7 +19,7 @@ return {
       return math.floor(vim.o.lines * 0.75)
     end,
     max_width = function()
-      return math.floor(vim.o.columns * 0.33)
+      return math.floor(vim.o.columns * 0.69)
     end,
     on_open = function(win)
       vim.api.nvim_win_set_config(win, { zindex = 100 })
