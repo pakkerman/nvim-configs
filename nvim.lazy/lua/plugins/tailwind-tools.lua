c28940b8c8937e6612f690e8e7c73835614f02a9
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-04-11 10:11:52 +0800

    feat: add plugin tailwind-tools

---
+++ nvim.lazy/lua/plugins/tailwind-tools.lua
@@ -0,0 +1,20 @@
+return {
+  "luckasRanarison/tailwind-tools.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  opts = {
+    document_color = {
+      enabled = false, -- can be toggled by commands
+      kind = "inline", -- "inline" | "foreground" | "background"
+      inline_symbol = "󰝤 ", -- only used in inline mode
+      debounce = 200, -- in milliseconds, only applied in insert mode
+    },
+  },
+  keys = {
+    { "<leader>t", desc = "+Tailwind-Tools" },
+    { "<leader>ts", "<cmd>:TailwindSort<cr>", desc = "Sort Classes" },
+    { "<leader>tv", "<cmd>:TailwindConcealToggle<cr>", desc = "Toggle Class Conceal" },
+    { "<leader>tc", "<cmd>:TailwindColorToggle<cr>", desc = "Toggle Inline Colors" },
+    { "<leader>tn", "<cmd>:TailwindNextClass<cr>", desc = "Go to next class" },
+    { "<leader>tp", "<cmd>:TailwindPrevClass<cr>", desc = "Go to prev class" },
+  },
+}
