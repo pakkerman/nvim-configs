34bf2668a7d153520142d436e706b67f9fc96077
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-04-23 18:22:48 +0800

    refactor: update : to use <cmd> instead, so the command line wont flash on the screen

--- nvim.lazy/lua/config/keymaps.lua
+++ nvim.lazy/lua/config/keymaps.lua
@@ -1,20 +1,23 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<leader><CR>", ":w<CR>:so %<CR>")
-vim.keymap.set("n", "<leader>\\", ":edit ~/.config/nvim/TODO.md<CR>")
+vim.keymap.set("n", "<leader><CR>", "<cmd>w<CR><cmd>so %<CR>")
+vim.keymap.set("n", "<leader>\\", "<cmd>edit ~/.config/nvim/TODO.md<CR>")
 vim.keymap.set("i", "…", "<esc>")
 
 -- Move Line up and down using opt-j and opt-k
-vim.keymap.set("v", "∆", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "˚", ":m '<-2<CR>gv=gv")
-vim.keymap.set("n", "∆", "ddp")
-vim.keymap.set("n", "˚", "o<esc>kkddpk")
+-- vim.keymap.set("v", "M-j", ":m '>+1<CR>gv=gv")
+-- vim.keymap.set("v", "M-k", ":m '<-2<CR>gv=gv")
+-- vim.keymap.set("n", "˚", "o<esc>kkddpk")
+-- vim.keymap.set("n", "∆", "ddp")
 
 -- Remap right-shift(which is remapped by karabiner to delete forward, which is the key ctrl + D)
 -- to <Del> key
 vim.keymap.set("i", "<C-D>", "<Del>")
 vim.keymap.set("i", "jk", "<esc>")
+vim.keymap.set("i", "jU", "<esc>")
+vim.keymap.set("i", "Jk", "<esc>")
+vim.keymap.set("i", "JK", "<esc>")
 
 -- Use option 1,2,3 to switch tabs
 vim.keymap.set("n", "<leader>`", "<C-w>s<C-w>T")
