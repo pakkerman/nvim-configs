a9e54cd8c6b4e2bbf25d9f741358f6ac90cd3397
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-12-05 10:24:05 +0800

    fix: dashboard.nvim find file action error

--- nvim.lazy/lua/plugins/dashboard.lua
+++ nvim.lazy/lua/plugins/dashboard.lua
@@ -24,11 +24,11 @@ return {
         header = vim.split(logo, "\n"),
         -- stylua: ignore
         center = {
-          { action = LazyVim.pick.telescope("files"),                            desc = " Find file",       icon = " ", key = "f" },
-          { action = "ene | startinsert",                                        desc = " New file",        icon = " ", key = "n" },
-          { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
-          { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
-          { action = [[lua LazyVim.telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
+          { action = 'lua LazyVim.pick()()',                                     desc = " Find file",       icon = " ", key = "f" },
+          { action = 'ene | startinsert',                                        desc = " New file",        icon = " ", key = "n" },
+          { action = 'lua LazyVim.pick("oldfiles")()',                                       desc = " Recent files",    icon = " ", key = "r" },
+          { action = 'lua LazyVim.pick("live_grep")()',                                      desc = " Find text",       icon = " ", key = "g" },
+          { action = 'lua LazyVim.pick.config_files()()',                        desc = " Config",          icon = " ", key = "c" },
           { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
           { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
           { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
