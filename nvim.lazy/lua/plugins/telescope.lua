076fedb07b27e50b6f18231ab54e5bd1bf87be02
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-12-25 10:08:02 +0800

    feat: add oil.nvim

--- nvim.lazy/lua/plugins/telescope.lua
+++ nvim.lazy/lua/plugins/telescope.lua
@@ -4,9 +4,10 @@ return {
     -- disable the keymap to grep files
     { "<leader><space>", false },
     -- change a keymap
-    { "<leader>p", "<cmd>Telescope git_files<cr>", desc = "Find Files" },
+    { "<leader>p", "<cmd>Telescope git_files<cr>", desc = "Git Files" },
     { "<leader>r", "<cmd>Telescope resume<cr>", desc = "Resume" },
   },
+
   opts = {
     defaults = {
       layout_strategy = "horizontal",
@@ -15,6 +16,10 @@ return {
       winblend = 0,
     },
     pickers = {
+      buffers = {
+        theme = "ivy",
+      },
+
       git_files = {
         -- Use git sparse-checkout list to get the sparse-checked-out files
         git_command = { "git", "ls-files", "--sparse" },
