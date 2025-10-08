3fc5e899aac72149b972d33866f8a171a9945368
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-10-08 14:49:59 +0800

    update: lazyvim

--- nvim.lazy/lua/plugins/oil.lua
+++ nvim.lazy/lua/plugins/oil.lua
@@ -3,9 +3,9 @@ return {
     "stevearc/oil.nvim",
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    opts = { view_options = { show_hidden = true } },
     -- Optional dependencies
-    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
     --
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }),
---
+++ nvim.lazy/lua/plugins/snacks.nvim.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    scroll = {
+      enabled = false,
+    },
+  },
+}
